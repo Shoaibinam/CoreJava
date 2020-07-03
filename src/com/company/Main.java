b4dd7f4fce4eb1e5ccd487baab1348da76a811d3
shoheb <shbtechno@gmail.com>
2020-07-03 17:28:24 +0100

    Added new print line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("This is my first project");
+        System.out.println("This is my second commit");
     }
 }
