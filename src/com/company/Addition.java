ded1d283afabaa2acb9731f32d4bfe06e5438bbd
shoheb <shbtechno@gmail.com>
2020-07-03 17:31:17 +0100

    Added new class

---
+++ src/com/company/Addition.java
@@ -0,0 +1,14 @@
+package com.company;
+/*
+   @Author      : Shoheb
+   Description  : 
+   >>>>>>>>>>>>>>>>>>>>> Annotations >>>>>>>>>>>>>>>>>>>>>  
+   @      :   
+   @      :   
+   
+ */
+
+public class Addition {
+
+    String addition="addition operation";
+}
